506a99be3681b17f4d3fdbb1b468a8b74f11d969
shivean <shivaramm222@gmail.com>
2025-07-09 23:12:56 +0545

    Update settings

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'users',
+    'predictions'
 ]
 
 MIDDLEWARE = [
@@ -74,11 +77,16 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'insurance_prediction_system',
+        'USER': 'postgres',
+        'PASSWORD': 'postgre', 
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
+AUTH_USER_MODEL = 'users.CustomUser'
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
