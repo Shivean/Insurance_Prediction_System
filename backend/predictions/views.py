cc57c9d246b4d1f4faa385abe2d54c8e2c7854ee
shivean <shivaramm222@gmail.com>
2025-07-10 14:29:11 +0545

    Fix the code

--- backend/predictions/views.py
+++ backend/predictions/views.py
@@ -1,22 +1,27 @@
 from django.shortcuts import render
-from rest_framework import serializers, response, status
-from rest_framework.decorators import api_view
+from rest_framework import serializers, status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from .serializers import HealthInsuranceSerializer, CarInsuranceSerializer
-import joblib
-from .models import PredictionHistory
+import joblib, numpy as np
+from .models import PredictionHistory, CustomUser
 
 health_prediction_model = joblib.load('ml_model/health_prediction_model.pkl')
 car_predicition_model = joblib.load('ml_model/car_insurance_prediction_model.pkl')
 
 # Create your views here.
-@api_view
+@api_view(['POST'])
+@permission_classes([IsAuthenticated]) # Permission class is IsAuthenticated
 def HealthInsurancePrediction(request):
     serializers = HealthInsuranceSerializer(data = request.data)
 
     if serializers.is_valid():
         prediction_data = serializers.validated_data
 
-        ml_input = {
+        # default_user = CustomUser.objects.get(username = 'shivaram@gmail.com') # For API testing purpose
+        
+        input_data = {  # Input data is organized into a dictionary since 'input_data' is a JSONField in the model
             'age': prediction_data['age'],
             'height': prediction_data['height'],
             'weight': prediction_data['weight'],
@@ -29,37 +34,53 @@ def HealthInsurancePrediction(request):
             'no_of_major_surgery': prediction_data['no_of_major_surgery']
         }
 
+        # This is the input data for the ML model in 2D array format
+        ml_input = ([[
+            prediction_data['age'],
+            prediction_data['height'],
+            prediction_data['weight'],
+            prediction_data['has_diabetes'],
+            prediction_data['has_pressure_issue'],
+            prediction_data['any_bodytransplant'],
+            prediction_data['any_chronic_disease'],
+            prediction_data['any_allergies'],
+            prediction_data['history_of_cancer_in_family'],
+            prediction_data['no_of_major_surgery']
+        ]])
+
         try:
-            predicted_health_premium = health_prediction_model.predict(ml_input)
+            predicted_health_premium = health_prediction_model.predict(ml_input) # Here, ML model performs prediction on given input
 
-            # Saving prediction to database
-            prediction = PredictionHistory.objects.create(
+            # Saving prediction to database (Table: PredictionHistory)
+            PredictionHistory.objects.create(
                 user = request.user,
+                input_data = input_data,
                 predicted_premium = predicted_health_premium,
-                insurance_type = 'health', 
-                **prediction_data)
-            
-            # response_serializer = HealthInsuranceSerializer(prediction)
+                insurance_type = 'health'
+                )
 
-            return response({
-                'message': predicted_health_premium
+            return Response({
+                'predicted_health_premium': predicted_health_premium
             }, status = status.HTTP_200_OK)
         except Exception as e:
-            return response({
+            return Response({
                     'error': f'Prediction failed: {str(e)}'
                 }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         
-    return response(serializers.errors, status = status.HTTP_400_BAD_REQUEST)
+    return Response(serializers.errors, status = status.HTTP_400_BAD_REQUEST)
 
 # For Car Insurance Prediction
-@api_view
+@api_view(['POST'])
+@permission_classes([IsAuthenticated]) # Permission class is IsAuthenticated
 def CarInsurancePrediction(request):
     serializers = CarInsuranceSerializer(data = request.data)
 
     if serializers.is_valid():
         prediction_data = serializers.validated_data
 
-        ml_input = {
+        # default_user = CustomUser.objects.get(username = 'shiva123@gmail.com') # For API testing purpose
+
+        input_data = { # Input data is organized into a dictionary since 'input_data' is a JSONField in the model
             'driver_age': prediction_data['driver_age'],
             'driving_experience': prediction_data['driving_experience'],
             'annual_mileage': prediction_data['annual_mileage'],
@@ -67,24 +88,32 @@ def CarInsurancePrediction(request):
             'prv_accident': prediction_data['prv_accident'],
         }
 
+        ml_input = ([[
+            prediction_data['driver_age'],
+            prediction_data['driving_experience'],
+            prediction_data['annual_mileage'],
+            prediction_data['car_mfg_year'],
+            prediction_data['prv_accident'],
+        ]])
+
         try:
             predicted_car_premium = car_predicition_model.predict(ml_input)
+            predicted_car_premium = round(predicted_car_premium[0], 2) # Round the result to two decimal places
 
             # Saving prediction to database
-
-            prediction = PredictionHistory.objects.create(
-                user = request.user, 
+            PredictionHistory.objects.create(
+                user = request.user,
+                input_data = input_data, 
                 predicted_premium = predicted_car_premium,
-                insurance_type = 'car', 
-                **prediction_data)
+                insurance_type = 'car')
             
-            return response({
-                'message': predicted_car_premium
+            return Response({
+                'predicted_car_premium': predicted_car_premium
                 }, status = status.HTTP_200_OK)
         
         except Exception as e:
-            return response({
+            return Response({
                 'error': f'Prediction failed: {str(e)}'
                 }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
     
-    return response(serializers.errors, status = status.HTTP_400_BAD_REQUEST)
+    return Response(serializers.errors, status = status.HTTP_400_BAD_REQUEST)
